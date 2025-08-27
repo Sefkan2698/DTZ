44b5f02db454aa33b0ece3f53430b6423eac2403
31548 <31548@students.hsrw>
2025-08-27 15:55:28 +0200

    s

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,39 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './pages/homepage/homepage';
-import { Baufinanzierung } from './pages/baufinanzierung/baufinanzierung';
-import { RatenkreditComponent } from './pages/ratenkredit/ratenkredit';
-import { ContactComponent } from './pages/contact/contact';
-import { DatenschutzComponent } from './pages/datenschutz/datenschutz';
-import { ImpressumComponent } from './pages/impressum/impressum';
-import { Bausparvertrag } from './pages/bausparvertrag/bausparvertrag';
-import { Tippgeber } from './pages/tippgeber/tippgeber';
-import { Component } from '@angular/core';
 
 export const routes: Routes = [
+  // Homepage sofort laden - kritisch für LCP
   { path: '', component: Homepage },
-  { path: 'baufinanzierung', component: Baufinanzierung },
-  { path: 'ratenkredit', component: RatenkreditComponent },
-  { path: 'contact', component: ContactComponent},
-  { path: 'datenschutz', component: DatenschutzComponent},
-  { path: 'impressum', component: ImpressumComponent},
-  { path: 'tippgeber', component: Tippgeber},
-  { path: 'bausparvertrag', component: Bausparvertrag},
-  // Weitere Routen hier...
+  
+  // Sekundäre Seiten lazy laden
+  { 
+    path: 'baufinanzierung', 
+    loadComponent: () => import('./pages/baufinanzierung/baufinanzierung').then(m => m.Baufinanzierung)
+  },
+  { 
+    path: 'ratenkredit', 
+    loadComponent: () => import('./pages/ratenkredit/ratenkredit').then(m => m.RatenkreditComponent)
+  },
+  { 
+    path: 'contact', 
+    loadComponent: () => import('./pages/contact/contact').then(m => m.ContactComponent)
+  },
+  { 
+    path: 'bausparvertrag', 
+    loadComponent: () => import('./pages/bausparvertrag/bausparvertrag').then(m => m.Bausparvertrag)
+  },
+  { 
+    path: 'tippgeber', 
+    loadComponent: () => import('./pages/tippgeber/tippgeber').then(m => m.Tippgeber)
+  },
+  
+  // Legal Pages - sehr selten besucht, definitiv lazy laden
+  { 
+    path: 'datenschutz', 
+    loadComponent: () => import('./pages/datenschutz/datenschutz').then(m => m.DatenschutzComponent)
+  },
+  { 
+    path: 'impressum', 
+    loadComponent: () => import('./pages/impressum/impressum').then(m => m.ImpressumComponent)
+  }
 ];
